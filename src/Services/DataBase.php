9644fe6114eb4e720d9e246547dc644aba2bf7fa
MojtabaSafarkhani <mojtabasfr01@gmail.com>
2023-02-11 16:26:21 +0330

    deleted blank line in DataBase file

--- src/Services/DataBase.php
+++ src/Services/DataBase.php
@@ -13,14 +13,10 @@ class DataBase
     public function __construct()
     {
         $config = new Config();
-
         try {
             $this->pdo = new PDO("mysql:host=" . $config->getDbHost() . ";dbname=" . $config->getDbName(), $config->getDbUserName(), $config->getDbPassword());
-
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
         } catch (Exception $e) {
-
             echo $e->getMessage();
         }
     }
